5728ba7b3692a03d86038cb6921eaf7de0f104bd
Bailee Strait <143550161+baileestrait@users.noreply.github.com>
2024-11-06 17:37:47 -0600

    Delete read-data.js

--- read-data.js
+++
@@ -1,8 +0,0 @@
-
-$("button").click(function(){
-    $.getJSON("https://barrycumbie.github.io/barrycumbie/data.json", function(result){
-      $.each(result, function(i, field){
-        $("div").append(field + " ");
-      });
-    });
-  });
