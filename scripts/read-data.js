4c7e8b4d8e0e40946ae4786a8d89d156f38da1ce
Bailee Strait <143550161+baileestrait@users.noreply.github.com>
2024-11-06 17:37:39 -0600

    Update read-data.js

--- scripts/read-data.js
+++ scripts/read-data.js
@@ -1,1 +1,7 @@
-
+$("button").click(function(){
+    $.getJSON("https://barrycumbie.github.io/barrycumbie/data.json", function(result){
+      $.each(result, function(i, field){
+        $("div").append(field + " ");
+      });
+    });
+  });
